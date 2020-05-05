76acf986e498b4f30723b6107426db1cb4ff8ca7
Eleandro Duzentos <eleandro@inbox.ru>
2020-05-05 19:05:32 +0100

    Added add method

--- lib/service_provider.dart
+++ lib/service_provider.dart
@@ -2,6 +2,18 @@ library service_provider;
 
 import 'ioc.dart';
 
-abstract class ServiceProvider {
+class ServiceProvider {
+  static final _providers = [];
+
+  ServiceProvider add(ServiceProviderContract serviceProvider, [String name]) {
+    _providers.add(serviceProvider);
+
+    return this;
+  }
+
+  void dispose() {}
+}
+
+abstract class ServiceProviderContract {
   void register(IoC container);
 }
