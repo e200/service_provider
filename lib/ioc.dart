e81879db3bdd433ea7e2081f480af7436a95c4d9
Eleandro Duzentos <eleandro@inbox.ru>
2020-05-05 18:53:29 +0100

    Added base methods

--- lib/ioc.dart
+++ lib/ioc.dart
@@ -1,3 +1,41 @@
+import 'package:get_it/get_it.dart';
+
 class IoC {
-  //
+  static IoC container = IoC();
+
+  static final _getIt = GetIt.I;
+
+  T get<T>() {
+    return _getIt.get<T>();
+  }
+
+  void register<T>(instance) {
+    _getIt.registerSingleton<T>(instance);
+  }
+
+  void registerAsync<T>(Future<T> Function() factoryFunction) {
+    _getIt.registerSingletonAsync<T>(factoryFunction);
+  }
+
+  void registerWithDependence<T>(
+    Function() factoryFunction, {
+    Iterable<Type> dependsOn,
+  }) {
+    _getIt.registerSingletonWithDependencies<T>(
+      factoryFunction,
+      dependsOn: dependsOn,
+    );
+  }
+
+  void registerFactory<T>(T Function() factoryFunction) {
+    _getIt.registerFactory<T>(factoryFunction);
+  }
+
+  void registerFactoryAsync<T>(Future<T> Function() factoryFunction) {
+    _getIt.registerFactoryAsync<T>(factoryFunction);
+  }
+
+  static void onReady(Function callback) {
+    _getIt.allReady().then(callback);
+  }
 }
