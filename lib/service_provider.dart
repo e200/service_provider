b8bf568a032e98ef374599b6f5fb6468faf6fd8b
Eleandro Duzentos <eleandro@inbox.ru>
2020-05-06 14:47:39 +0100

    Removed add method Now boot is async so we can do other things while dependencies are booting

--- lib/service_provider.dart
+++ lib/service_provider.dart
@@ -5,17 +5,13 @@ import 'package:get_it/get_it.dart';
 part 'ioc.dart';
 
 class ServiceProviders {
-  static final _providers = [];
+  final List<ServiceProvider> _providers;
+
+  ServiceProviders(this._providers);
 
   static final _container = IoC.container;
 
-  ServiceProviders add(ServiceProvider serviceProvider) {
-    _providers.add(serviceProvider);
-
-    return this;
-  }
-
-  static boot() async {
+  Future<void> boot() async {
     _providers.forEach((_provider) {
       _provider.register(_container);
     });
